d499ca11d79c903dadc0f81ce62fe9dc828d50f4
angelorub94 <angelo.rubino94@gmail.com>
2015-12-01 10:52:32 +0100

    finished

--- app.js
+++ app.js
@@ -9,24 +9,31 @@ $(document).ready(function(){
 var model = {
     collection: data,
     summary: [],
-    getSummary: function(){
+    getSummary: function(cittaselez){
+        summary = [];
         for (var i = 0; i<this.collection.length; i++){
-            //controllo se il giorno esiste
+            //controllo se il giorno esiste  
+           // alert(JSON.stringify(this.summary[this.collection[i].day]));
             if(summary[this.collection[i].day] == undefined){
-        //se non esiste ne creo uno con min e max settati
-              summary[this.collection[i].day] = {
-                min: this.collection[i].temperature,
-                max: this.collection[i].temperature,
-                day: this.collection[i].day,  
-                cond: this.collection[i].condition,
-              };
+            //se non esiste ne creo uno con min e max settati
+                if(this.collection[i].city == cittaselez){
+                    summary[this.collection[i].day] = {
+                    min: this.collection[i].temperature,
+                    max: this.collection[i].temperature,
+                    day: this.collection[i].day, 
+                    //city: this.collection[i].city,    
+                    cond: this.collection[i].condition,
+                    };
+                }
             } else {
-                if(summary[this.collection[i].day].min>this.collection[i].temperature){
-                   summary[this.collection[i].day].min = this.collection[i].temperature;
-                   }
-                if(summary[this.collection[i].day].max<this.collection[i].temperature){
-                   summary[this.collection[i].day].max = this.collection[i].temperature;
-                   }
+                    if(this.collection[i].city == cittaselez){
+                        if(summary[this.collection[i].day].min>this.collection[i].temperature){
+                            summary[this.collection[i].day].min = this.collection[i].temperature;
+                        }
+                        if(summary[this.collection[i].day].max<this.collection[i].temperature){
+                            summary[this.collection[i].day].max = this.collection[i].temperature;
+                        }
+                    }
               }
         }
         return summary;
@@ -35,21 +42,37 @@ var model = {
     
 var controller = {
     init: function(){
-        view.summary();
+        //view.summary();
+        viewFilter.selCity();
     }, 
     
-    getSummary: function(){
-        return model.getSummary();
+    getSummary: function(citta){
+        return model.getSummary(citta);
     }
 };
     
-var view = {
-    summary: function(){
-        var list = controller.getSummary();   
-        
+/*-----VIEWS------*/
+
+var viewFilter = {
+    selCity: function(){
+        $("#btn-filter").click(function(){
+            if($(".city").val()!= ""){
+               view.summary($(".city").val()); 
+            }
+            else {
+                alert("Select a city");
+            }
+        })
+    }
+};
+
+var view = {    
+    summary: function(citta){
+        var list = controller.getSummary(citta);   
+        alert (JSON.stringify(list));
         //mi serve un vettore in cui salvo i diversi giorni.
         var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-        
+        $("#summary").html("");
         for(i=0; i<days.length;i++){
             if(list[days[i]] != undefined){
             var temp = tmpl.replace("IMG",list[days[i]].cond).replace("DAY",list[days[i]].day).replace("VALMIN",list[days[i]].min).replace("VALMAX",list[days[i]].max);
